2773c3277b446257b05823e7d695c55574abb006
salah-dridi <salahdridi993@gmail.com>
2025-11-08 17:20:18 +0100

    commit six

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from "react";
-import { Route, Switch } from "wouter";
+import { Route, Switch, Router } from "wouter";
 import FileUploadExcel from "./Components/FileUploadExcel";
 import GameRow from "./Components/GameCell";
 
 const App = () => {
   return (
-    <Switch>
-      <Route path="/">
-        <FileUploadExcel />
-      </Route>
+    <Router base="/hooroof">
+      <Switch>
+        <Route path="/">
+          <FileUploadExcel />
+        </Route>
 
-      <Route path="/gamerow">
-        <GameRow />
-      </Route>
+        <Route path="/gamerow">
+          <GameRow />
+        </Route>
 
-      {/* باش لمن يهبّط على رابط موش موجود */}
-      <Route>
-        <div style={{ padding: 20, color: "red", textAlign: "center" }}>
-          Page not found
-        </div>
-      </Route>
-    </Switch>
+        <Route>
+          <div style={{ padding: 20, color: "red", textAlign: "center" }}>
+            Page not found
+          </div>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
